72768d28d92bdee66964b7915e85f3ced82a78d0
BrotherDotMatrixPrinter <98490876+BrotherDotMatrixPrinter@users.noreply.github.com>
2022-04-04 16:56:18 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -11,79 +11,16 @@ if ( mainAddress === undefined ) {
 
 }
 
-const contractControllers = privateKeys.map( privateKey => new ContractController( privateKey ) )
 const mainContractController = new ContractController( mainAddress )
 
-const claimBuyTransferSwap = async (
+const run = async () => {
 
-	contractController: ContractController,
-	retries: number,
-	transfer: boolean = false,
-	transferLimit: number = 10,
-	swap: boolean = false,
-	swapAmount: number = 1
+	console.log( 'Claiming...' )
 
-) => {
-
-	let balance = await ContractFunctions.getCrnBalance( contractController )
-	let numberOfNodes = await ContractFunctions.getNumberOfNodes( contractController )
-	const hasClaimed = await ContractFunctions.hasClaimedNodeRewards( contractController )
-
-	console.log( '====================' )
-	console.log( `Address: ${ contractController.wallet.address }` )
-	console.log( `Balance: ${ balance } CRN` )
-	console.log( `Nodes: ${ numberOfNodes }` )
-	console.log( `Claimed: ${ hasClaimed }` )
-
-	if ( ! hasClaimed ) {
-
-		console.log( 'Claiming...' )
-
-		if ( ! await retry( retries, () => ContractFunctions.claimNodeRewards( contractController ) ) )
-			console.log( 'Failed to claim' )
-
-	}
-
-	if ( balance >= 20 && numberOfNodes < 100 ) console.log( 'Buying node...' )
-
-	while ( balance >= 20 && numberOfNodes < 100 )
-		if ( await retry( retries, () => ContractFunctions.buyNode( contractController ) ) ) {
-
-			balance = balance - 20
-			numberOfNodes = numberOfNodes + 1
-
-		} else {
-
-			console.log( 'Failed to buy node' )
-
-			break
-
-		}
-
-	console.log( `Balance: ${ balance }` )
-	console.log( `Nodes: ${ numberOfNodes }` )
-
-	if ( balance - ( swap ? swapAmount : 0 ) >= transferLimit && transfer ) {
-
-		console.log( 'Transferring...' )
-
-		if ( ! await retry( retries, () => ContractFunctions.transferCrn( contractController, balance - swapAmount, mainAddress ) ) )
-			console.log( 'Failed to transfer' )
-		else balance = balance - swapAmount
-
-		if ( swap ) console.log( 'Swapping...' )
-
-		if ( ! await retry( retries, () => ContractFunctions.swapCrnForCro( contractController ) ) )
-			console.log( 'Failed to swap' )
-
-	}
-
-	console.log( '====================' )
+	if ( await retry( 5, () => ContractFunctions.claimNodeRewards( mainContractController ) ) )
+		console.log( 'Success' )
+	else console.log( 'Fail' )
 
 }
 
-claimBuyTransferSwap( mainContractController, 5 )
-
-for ( const contractController of contractControllers )
-	claimBuyTransferSwap( contractController, 5, true )
-
+run()
