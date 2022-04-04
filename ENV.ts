0a58048d276efbf8be43c818a18cd1f0ab19ad34
BrotherDotMatrixPrinter <98490876+BrotherDotMatrixPrinter@users.noreply.github.com>
2022-04-03 22:37:46 -0400

    init commit

---
+++ ENV.ts
@@ -0,0 +1,5 @@
+export default {
+	NODE_NAME: 'roflmao',
+	CRONOS_RPC_URL: 'https://evm.cronos.org/',
+	MAX_RETRIES: 5
+}
---
+++ index.ts
@@ -0,0 +1,4 @@
+import { claimAndBuy } from './ethers_util/Functions'
+import PrivateKeys from './PrivateKeys'
+
+claimAndBuy( PrivateKeys[ 0 ] as string )
